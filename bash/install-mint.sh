86233bcfe17ea72f6386e6c6b9a9d483ab163bad
Andrey Kharanenka <a.khoronenko@lovata.com>
2018-12-04 14:21:57 +0300

    Added docker files from laradock

---
+++ bash/install-mint.sh
@@ -0,0 +1,20 @@
+#Install docker
+sudo apt-get remove docker docker-engine docker.io
+sudo apt-get update
+sudo apt-get install -y apt-transport-https ca-certificates curl software-properties-common
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+sudo apt-key fingerprint 0EBFCD88
+sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu xenial stable"
+sudo apt-get update
+sudo apt-get install -y docker-ce
+sudo apt-get install -y docker-compose
+
+#Install ansible
+sudo apt-add-repository -y ppa:ansible/ansible
+sudo apt-get update
+sudo apt-get install -y ansible
+
+#Install make
+sudo apt-get install -y make
+
+sudo usermod -a -G docker $USER
